60501eda61ca552b5db3e20df1d06c7686992a5b
godChoi96 <oinkoink71@naver.com>
2022-06-07 21:11:11 +0900

    2022.06.07 - Delete API

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'api'
 urlpatterns = [
     path('todo/list/', views.ListAPIView.as_view(), name="list"),
-    # path('todo/<int:pk>/delete', views.DeleteAPIView.as_view(), name="delete"),
+    path('todo/<int:pk>/delete/', views.DeleteAPIView.as_view(), name="delete"),
     # path('todo/create/', views.CreateAPIView.as_view(), name="create"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.generic import ListView
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect, csrf_exempt
+from django.views.generic import ListView, DeleteView
 
 from todo.models import Todo
 
@@ -17,4 +17,21 @@ class ListAPIView(ListView):
 
     def render_to_response(self, context, **response_kwargs):
         todoList = list(context['object_list'].values())
+        print(todoList)
         return JsonResponse(data=todoList, safe=False)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteAPIView(DeleteView):
+    # DeleteAPIView 클래스의 의도
+    """
+    클라이언트 단에서는 삭제하였으나 서버 단에서는 삭제되어있지 않는다.
+    따라서 똑같이 DB에서 삭제될 수 있도록 DeleteView의 delete 메소드를 오버라이딩한다.
+    """
+
+    model = Todo
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return JsonResponse(data={}, status=204)
