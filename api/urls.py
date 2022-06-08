acb1cd0704675dcb6e3bd88047d56082ed039a01
godChoi96 <oinkoink71@naver.com>
2022-06-08 22:16:17 +0900

    2022.06.08 - Create TodoList

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,5 @@ app_name = 'api'
 urlpatterns = [
     path('todo/list/', views.ListAPIView.as_view(), name="list"),
     path('todo/<int:pk>/delete/', views.DeleteAPIView.as_view(), name="delete"),
-    # path('todo/create/', views.CreateAPIView.as_view(), name="create"),
+    path('todo/create/', views.CreateAPIView.as_view(), name="create"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
+import json
+
+from django.forms import model_to_dict
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect, csrf_exempt
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DeleteView, CreateView
 
 from todo.models import Todo
 
@@ -17,7 +20,7 @@ class ListAPIView(ListView):
 
     def render_to_response(self, context, **response_kwargs):
         todoList = list(context['object_list'].values())
-        print(todoList)
+        # print(todoList)
         return JsonResponse(data=todoList, safe=False)
 
 
@@ -35,3 +38,28 @@ class DeleteAPIView(DeleteView):
         self.object = self.get_object()
         self.object.delete()
         return JsonResponse(data={}, status=204)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CreateAPIView(CreateView):
+    model = Todo
+    fields = '__all__'
+
+    def get_form_kwargs(self):
+
+        kwargs = super().get_form_kwargs()
+        # print(kwargs)
+        kwargs['data'] = json.loads(self.request.body)
+        # print(kwargs)
+        return kwargs
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        # print(form)
+        self.object = form.save()
+        createdTodo = model_to_dict(self.object)
+        print(createdTodo)
+        return JsonResponse(data=createdTodo, status=201)
+
+    def form_invalid(self, form):
+        return JsonResponse(data=form.erros, status=400)
